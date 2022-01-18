359db48b18d4f1ab50de2e848503ac4515a0b51e
NotExisting <61290107+N0tExisting@users.noreply.github.com>
2022-01-18 18:57:31 +0100

    chore: add postbuild

---
+++ scripts/post-build.js
@@ -0,0 +1,44 @@
+// @ts-check
+import { join, dirname } from 'node:path';
+import * as fs from 'node:fs';
+import * as console from 'node:console';
+import rimraf from 'rimraf';
+
+const outDir = join(dirname(import.meta.url), '..', 'dist');
+
+const files = {
+	'src/index.d.ts': 'index.d.ts',
+	'src/vite/index.d.ts': 'vite.d.ts',
+	'src/bin/index.d.ts': 'bin.d.ts',
+};
+
+/** @param {string} path */
+function fixPath(path) {
+	if (path.startsWith('file:\\')) {
+		path = path.slice('file:\\'.length);
+	}
+	return path;
+}
+
+/** @type {false | Error} */
+let error = false;
+for (const [src, dest] of Object.entries(files)) {
+	const srcPath = fixPath(join(outDir, src));
+	const destPath = fixPath(join(outDir, dest));
+	//console.log(`'${srcPath}' -> '${destPath}'`);
+	try {
+		fs.copyFileSync(srcPath, destPath);
+	} catch (err) {
+		console.error(`Error copying file '${src}' → '${dest}'`);
+		// @ts-expect-error - we know this is an Error
+		error = err;
+		break;
+	}
+}
+
+if (error) {
+	throw error;
+} else {
+	rimraf.sync(outDir);
+	console.log('Copied types');
+}
